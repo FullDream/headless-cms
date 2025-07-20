40f3d0174d7f713199919007b97de40025e1af41
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-20 19:19:06 +0300

    feat(infrastructure, webApi): configure EF Core, SQLite and ContentType entity

---
+++ src/Infrastructure/ContentTypes/ContentTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.ContentTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.ContentTypes;
+
+public class ContentTypeConfiguration: IEntityTypeConfiguration<ContentType>
+{
+
+	public void Configure(EntityTypeBuilder<ContentType> builder)
+	{
+		builder.HasKey(ct => ct.Id);
+
+		builder.Property(ct => ct.Name).IsRequired();
+		builder.HasIndex(ct => ct.Name).IsUnique();
+
+		builder.Property(ct => ct.Kind).IsRequired();
+	}
+	
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Core.ContentTypes;
+using Infrastructure.ContentTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddDbInfrastructure(
+		this IServiceCollection services,
+		IConfiguration config)
+	{
+		services.AddDbContext<AppDbContext>(options =>
+				options.UseSqlite(config.GetConnectionString("DefaultConnection")))
+			.AddScoped<IContentTypeRepository, ContentTypeRepository>();
+
+		return services;
+	}
+}
