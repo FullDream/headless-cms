33b75898752baf28152928bd07be91f1bf1684e2
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-20 20:03:42 +0300

    feat(content-type): implement ContentTypeRepository methods

---
+++ src/Infrastructure/ContentTypes/ContentTypeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Core.ContentTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.ContentTypes;
+
+public class ContentTypeRepository(AppDbContext dbContext) : IContentTypeRepository
+{
+	public async Task<IEnumerable<ContentType>> FindManyAsync(ContentTypeKind? kind = null,
+		CancellationToken cancellationToken = default)
+	{
+		return await dbContext.ContentTypes
+			.AsNoTracking()
+			.Where(ct => kind == null || ct.Kind == kind)
+			.ToListAsync(cancellationToken);
+	}
+
+	public async Task<ContentType?> FindByNameAsync(string name, CancellationToken cancellationToken = default)
+	{
+		return await dbContext.ContentTypes.AsNoTracking()
+			.FirstOrDefaultAsync(ct => ct.Name == name, cancellationToken);
+	}
+
+	public async Task<ContentType?> FindByIdAsync(Guid id, CancellationToken cancellationToken = default)
+	{
+		return await dbContext.ContentTypes.FindAsync([id], cancellationToken);
+	}
+
+	public async Task<ContentType> AddAsync(ContentType contentType, CancellationToken cancellationToken = default)
+	{
+		dbContext.ContentTypes.Add(contentType);
+		await dbContext.SaveChangesAsync(cancellationToken);
+
+		return contentType;
+	}
+
+	public async Task<ContentType> UpdateAsync(ContentType contentType, CancellationToken cancellationToken = default)
+	{
+		dbContext.ContentTypes.Update(contentType);
+		await dbContext.SaveChangesAsync(cancellationToken);
+
+		return contentType;
+	}
+
+	public async Task<ContentType?> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+	{
+		var entity = await dbContext.ContentTypes.FindAsync([id], cancellationToken);
+
+		if (entity == null) return null;
+
+		dbContext.ContentTypes.Remove(entity);
+		await dbContext.SaveChangesAsync(cancellationToken);
+
+		return entity;
+	}
+}
