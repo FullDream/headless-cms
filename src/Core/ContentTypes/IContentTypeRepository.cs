58fb5a70479fb7b822cf0e0321565908b726830c
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-20 20:03:26 +0300

    feat(content-type): add cancellationToken to methods

--- src/Core/ContentTypes/IContentTypeRepository.cs
+++ src/Core/ContentTypes/IContentTypeRepository.cs
@@ -2,10 +2,10 @@
 
 public interface IContentTypeRepository
 {
-	Task<IEnumerable<ContentType>> FindManyAsync(ContentTypeKind? kind = null);
-	Task<ContentType?> FindByNameAsync(string name);
-	Task<ContentType?> FindByIdAsync(Guid id);
-	Task<ContentType> AddAsync(ContentType contentType);
-	Task<ContentType> UpdateAsync(ContentType contentType);
-	Task<ContentType?> DeleteAsync(Guid id);
+	Task<IEnumerable<ContentType>> FindManyAsync(ContentTypeKind? kind = null,  CancellationToken cancellationToken = default);
+	Task<ContentType?> FindByNameAsync(string name,  CancellationToken cancellationToken = default);
+	Task<ContentType?> FindByIdAsync(Guid id,  CancellationToken cancellationToken = default);
+	Task<ContentType> AddAsync(ContentType contentType,  CancellationToken cancellationToken = default);
+	Task<ContentType> UpdateAsync(ContentType contentType,  CancellationToken cancellationToken = default);
+	Task<ContentType?> DeleteAsync(Guid id,  CancellationToken cancellationToken = default);
 }
