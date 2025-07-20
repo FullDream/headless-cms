1f1144de16b57ed35bbd8bd6f5f1eeee63f33eee
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-20 22:15:29 +0300

    feat(core): add methods

--- src/Core/ContentTypes/ContentType.cs
+++ src/Core/ContentTypes/ContentType.cs
@@ -5,4 +5,14 @@ public class ContentType(Guid id, string name, ContentTypeKind kind)
 	public Guid Id { get; private set; } = id;
 	public string Name { get; private set; } = name;
 	public ContentTypeKind Kind { get; private set; } = kind;
+
+	public void Rename(string name)
+	{
+		Name = name;
+	}
+
+	public void ChangeKind(ContentTypeKind kind)
+	{
+		Kind = kind;
+	}
 }
