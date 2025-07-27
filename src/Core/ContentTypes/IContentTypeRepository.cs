4ec9a92b5d941d177080fa3fe17c8cff6af8c13d
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-28 00:30:02 +0300

    feat(contentTypes): add explicit SaveChangesAsync to repository

--- src/Core/ContentTypes/IContentTypeRepository.cs
+++ src/Core/ContentTypes/IContentTypeRepository.cs
@@ -2,10 +2,13 @@
 
 public interface IContentTypeRepository
 {
-	Task<IEnumerable<ContentType>> FindManyAsync(ContentTypeKind? kind = null,  CancellationToken cancellationToken = default);
-	Task<ContentType?> FindByNameAsync(string name,  CancellationToken cancellationToken = default);
-	Task<ContentType?> FindByIdAsync(Guid id,  CancellationToken cancellationToken = default);
-	Task<ContentType> AddAsync(ContentType contentType,  CancellationToken cancellationToken = default);
-	Task<ContentType> UpdateAsync(ContentType contentType,  CancellationToken cancellationToken = default);
-	Task<ContentType?> DeleteAsync(Guid id,  CancellationToken cancellationToken = default);
+	Task<IEnumerable<ContentType>> FindManyAsync(ContentTypeKind? kind = null,
+		CancellationToken cancellationToken = default);
+
+	Task<ContentType?> FindByNameAsync(string name, CancellationToken cancellationToken = default);
+	Task<ContentType?> FindByIdAsync(Guid id, CancellationToken cancellationToken = default);
+	void Add(ContentType contentType);
+	void Update(ContentType contentType);
+	void Remove(ContentType contentType);
+	Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
