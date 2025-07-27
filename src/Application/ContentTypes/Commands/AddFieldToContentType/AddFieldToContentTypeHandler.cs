8c9e7397a3ed7dc1fdf8ce00af5426cc0706d82f
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-28 01:08:34 +0300

    feat(content-type): create AddFieldToContentType command and ContentField mapping

---
+++ src/Application/ContentTypes/Commands/AddFieldToContentType/AddFieldToContentTypeHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.ContentTypes.Dtos;
+using Application.ContentTypes.Mappers;
+using Core.ContentTypes;
+using MediatR;
+
+namespace Application.ContentTypes.Commands.AddFieldToContentType;
+
+public class AddFieldToContentTypeHandler(IContentTypeRepository repository)
+	: IRequestHandler<AddFieldToContentTypeCommand, ContentFieldDto>
+{
+	public async Task<ContentFieldDto> Handle(AddFieldToContentTypeCommand request, CancellationToken cancellationToken)
+	{
+		var contentType = await repository.FindByIdAsync(request.ContentTypeId, cancellationToken);
+
+		if (contentType is null) throw new InvalidOperationException($"ContentType  is not found");
+
+		var fieldRequest = request.Field;
+		var duplicate = contentType.Fields.FirstOrDefault(f => f.Name == fieldRequest.Name);
+
+		if (duplicate is not null)
+			throw new InvalidOperationException($"Field with name {request.Field.Name} already exists");
+
+		var field = contentType.AddField(fieldRequest.Name, fieldRequest.Label, fieldRequest.Type,
+			fieldRequest.IsRequired);
+
+		await repository.SaveChangesAsync(cancellationToken);
+
+		return field.ToDto();
+	}
+}
---
+++ src/Application/ContentTypes/Mappers/ContentFieldMapper.cs
@@ -0,0 +1,10 @@
+﻿using Application.ContentTypes.Dtos;
+using Core.ContentTypes;
+
+namespace Application.ContentTypes.Mappers;
+
+public static class ContentFieldMapper
+{
+	public static ContentFieldDto ToDto(this ContentField contentField) => new ContentFieldDto(contentField.Id,
+		contentField.Name, contentField.Label, contentField.Type, contentField.IsRequired);
+}
