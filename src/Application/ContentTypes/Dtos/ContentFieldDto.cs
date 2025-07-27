86efa99014f13afede8927faf7750423726e2481
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-28 00:16:41 +0300

    feat(contentTypes): add contentField dtos

---
+++ src/Application/ContentTypes/Dtos/ContentFieldDto.cs
@@ -0,0 +1,5 @@
+﻿using Core.ContentTypes;
+
+namespace Application.ContentTypes.Dtos;
+
+public record ContentFieldDto(Guid Id, string Name, string Label, FieldType Type, bool IsRequired = false);
---
+++ src/Application/ContentTypes/Dtos/CreateContentFieldDto.cs
@@ -0,0 +1,5 @@
+﻿using Core.ContentTypes;
+
+namespace Application.ContentTypes.Dtos;
+
+public record CreateContentFieldDto(string Name, string Label, FieldType Type, bool IsRequired = false);
