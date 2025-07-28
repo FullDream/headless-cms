5cbd100f136bb2dbf2cf08369a69c3a29449f636
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-28 23:25:01 +0300

    refactor(contentTypes): rename DeleteContentType to RemoveContentType

--- src/Application/ContentTypes/Commands/DeleteContentType/DeleteContentTypeCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Application.ContentTypes.Dtos;
-using MediatR;
-
-namespace Application.ContentTypes.Commands.DeleteContentType;
-
-public record DeleteContentTypeCommand(Guid Id) : IRequest<ContentTypeDto?>;
