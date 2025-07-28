650500920b289cf65ab20786440182e47ec74235
Pavel Rybakin <pvrybakin@gmail.com>
2025-07-28 23:39:51 +0300

    feat(contentTypes): add RemoveFieldFromContentType command and add return type in ContentType

---
+++ src/Application/ContentTypes/Commands/RemoveFieldFromContentType/RemoveFieldFromContentTypeCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.ContentTypes.Dtos;
+using MediatR;
+
+namespace Application.ContentTypes.Commands.RemoveFieldFromContentType;
+
+public record RemoveFieldFromContentTypeCommand(Guid ContentFieldId, Guid ContentTypeId) : IRequest<ContentFieldDto?>;
---
+++ src/Application/ContentTypes/Commands/RemoveFieldFromContentType/RemoveFieldFromContentTypeHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.ContentTypes.Dtos;
+using Application.ContentTypes.Mappers;
+using Core.ContentTypes;
+using MediatR;
+
+namespace Application.ContentTypes.Commands.RemoveFieldFromContentType;
+
+public class RemoveFieldFromContentTypeHandler(IContentTypeRepository repository)
+	: IRequestHandler<RemoveFieldFromContentTypeCommand, ContentFieldDto?>
+{
+	public async Task<ContentFieldDto?> Handle(RemoveFieldFromContentTypeCommand request,
+		CancellationToken cancellationToken)
+	{
+		var contentType = await repository.FindByIdAsync(request.ContentTypeId, cancellationToken);
+
+		if (contentType is null) throw new InvalidOperationException($"ContentType is not found");
+
+		var field = contentType.RemoveField(request.ContentFieldId);
+
+		if (field is null) return null;
+
+		await repository.SaveChangesAsync(cancellationToken);
+
+		return field.ToDto();
+	}
+}
