c2dd28b599c4ab4e855816a597153f1a98e25aac
Pavel Rybakin <pvrybakin@gmail.com>
2025-08-16 23:00:50 +0300

    chore: add and configure prime with tailwind

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,32 @@
-import {
-	ApplicationConfig,
-	provideBrowserGlobalErrorListeners,
-	provideZonelessChangeDetection,
-} from '@angular/core'
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { appRoutes } from './app.routes'
 import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental'
 import { provideHttpClient, withFetch } from '@angular/common/http'
+import { providePrimeNG } from 'primeng/config'
+import Aura from '@primeuix/themes/aura'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideZonelessChangeDetection(),
+		provideRouter(appRoutes),
 		provideHttpClient(withFetch()),
 		provideTanStackQuery(new QueryClient()),
-		provideRouter(appRoutes),
+		provideAnimationsAsync(),
+		providePrimeNG({
+			ripple: true,
+			theme: {
+				preset: Aura,
+				options: {
+					darkModeSelector: '.dark-mode',
+					cssLayer: {
+						name: 'primeng',
+						order: 'theme, base, primeng',
+					},
+				},
+			},
+		}),
 	],
 }
