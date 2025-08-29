2223f89e9445a4c930cc3d4381671391ee48dcbe
Pavel Rybakin <pvrybakin@gmail.com>
2025-08-29 22:41:08 +0300

    fix: return usings

--- backend/src/WebApi/Program.cs
+++ backend/src/WebApi/Program.cs
@@ -1,3 +1,9 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Application;
+using Infrastructure;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
