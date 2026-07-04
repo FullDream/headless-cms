f4adfcfc6a827f75aa0ff29a35165664f3a1a85f
Pavel Rybakin <pvrybakin@gmail.com>
2026-07-04 18:08:44 +0300

    chore: migrate to nx 23

--- frontend/libs/content-entries/feat-editor/src/index.ts
+++ frontend/libs/content-entries/feat-editor/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/content-entries-feat-editor/content-entry-editor'
 export { mapFieldToControlConfig } from './lib/content-entries-feat-editor/map-field-to-control-config'
-export { ContentEntryFormModel } from './lib/content-entries-feat-editor/content-entry.models'
-export { ContentEntryValue } from './lib/content-entries-feat-editor/content-entry.models'
+export type { ContentEntryFormModel } from './lib/content-entries-feat-editor/content-entry.models'
